efd2e6e37db5ef6cd79b470be9bc3422dffe34ea
Steve Miesch <mieschst@mail.gvsu.edu>
2015-09-19 20:25:59 -0400

    Attempting to re-commit Assets after improper deletion...

---
+++ Assets/C#Scripts/MainMenuScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public Button startButton;
+    public Button quitButton;
+
+    void MainGUI()
+    {
+        startButton = startButton.GetComponent<Button>();
+        quitButton = quitButton.GetComponent<Button>();
+
+        startButton.enabled = true;
+        quitButton.enabled = true;
+    }
+
+    public void StartGame()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
