c6f5dfc9d94387c3fe7fb66e3839aa94cbead80d
rootja <rootja@mail.gvsu.edu>
2015-09-27 21:43:25 -0400

    Added a really stupid AI using Cynthia, the Sinnoh champion.

--- Assets/C#Scripts/GameManager.cs
+++ Assets/C#Scripts/GameManager.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 
 	public GameObject player;
+	public GameObject cynthia;
 
 	// Use this for initialization
 	void Start () {
 		Instantiate (player);
+		Instantiate (cynthia);
 	}
 	
 	// Update is called once per frame
