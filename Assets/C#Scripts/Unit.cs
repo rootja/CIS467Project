d87e542f0d72b814191a733b39a8e638bdf4934b
Bill Baer <baerw@mail.gvsu.edu>
2015-09-28 22:43:10 -0400

    Unit Class With Contract for Player Class
    The Player class now extends from Unit and not UnitMovement. Each Unit
    must have a method for movement and also for their inventory. I moved
    the code within the Update method in the player class to the Player’s
    Move method. I then called Move() in Update().

---
+++ Assets/C#Scripts/Unit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Unit : MonoBehaviour {
+
+	// Every unit must have an algorith or method of moving on the board.
+	public abstract void Move();
+
+	// Every unit must have an inventory.
+	public abstract GameObject[] Inventory ();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
