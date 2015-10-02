3209c2e398f0a28f55c3769e27959446495978f0
Bill Baer <baerw@mail.gvsu.edu>
2015-10-02 05:58:13 -0400

    Added A Few Items and Updated BoardManager Logic
    I added logic to the BoardManager script that generates items and
    places them on the map at random locations. There are 4 items
    currently. A blue rupee (small), yellow rupee (medium), red rupee
    (large), and a heart. My next goal is to figure out how to handle
    triggers and update the player’s currency value.

---
+++ Assets/C#Scripts/Item.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Item : MonoBehaviour {
+
+	// Every item should have some sort of an effect when either acquired or used.
+	public abstract void Effect ();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
