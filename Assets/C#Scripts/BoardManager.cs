99b5b848a5db6d3130f184f1d36014f7961a88a4
Bill Baer <baerw@mail.gvsu.edu>
2015-10-03 22:25:13 -0400

    More Items and Player Animations
    I added in some more items, animations for the player, and also fixed
    up some logic pertaining to the random item placement on the board. The
    board is now 15x9.

--- Assets/C#Scripts/BoardManager.cs
+++ Assets/C#Scripts/BoardManager.cs
@@ -11,10 +11,13 @@ public class BoardManager : MonoBehaviour {
 
 	public GameObject[] items;
 
+	public GameObject ladder;
+
 	public int rows;
 	public int columns;
 
 	private Transform boardTiles;
+	private Transform boardItems;
 	
 	public void SetupBoard(int rows = 1, int columns = 1){
 
@@ -36,7 +39,7 @@ public class BoardManager : MonoBehaviour {
 		boardTiles = new GameObject ("BoardTiles").transform;
 
 		// Assigns values to the column and row variables.
-		SetupBoard (9, 9);
+		SetupBoard (15, 9);
 		
 		// Adds the floor tiles to the game board.
 		for (int i = 0; i < rows; i++) {
@@ -45,7 +48,7 @@ public class BoardManager : MonoBehaviour {
 				GameObject newTile = Instantiate(floorTile, new Vector2(i, j), Quaternion.identity) as GameObject;
 
 				// Adds the new tile to GameObject called 'BoardTiles' to help reduce clutter in the
-				// heirarchy.
+				// hierarchy.
 				newTile.transform.SetParent(boardTiles);
 			}
 		}
@@ -53,15 +56,19 @@ public class BoardManager : MonoBehaviour {
 		// Adds the wall tiles to the game board.
 		for (int i = -1; i <= rows; i++) {
 			for (int j = -1; j <= columns; j++){
-				if(i == -1 || i == columns || j == -1 || j == columns){
+				if(i == -1 || i == rows || j == -1 || j == columns){
 					GameObject newTile = Instantiate(wallTile, new Vector2(i,j), Quaternion.identity) as GameObject;
 					newTile.transform.SetParent(boardTiles);
 				}
 			}
 		}
 
-		// May generate up to 5 items on the board.
-		GenerateItems (5);
+		// Adds a ladder right corner of the moveable section of the board.
+		Instantiate (ladder, new Vector3 (rows-1, columns-1, -1), Quaternion.identity);
+
+		// May generate items up to the specified number and place them on the board.
+		GenerateItems (10);
+
 	}
 
 	// Generates an item and places it at some random position on the board. Note: The floor lining the wall
@@ -70,19 +77,26 @@ public class BoardManager : MonoBehaviour {
 		// Holds the positions of each item added to the game board.
 		List<Vector3> positions = new List<Vector3> ();
 
+		boardItems = new GameObject ("BoardItems").transform;
+
 		// Adds items at random positions on the board.
 		for (int i = 0; i < numberOfItems; i++) {
-			// Values between 1 and 8.
-			float x = (int)(Random.value * (columns-2)+1);
-			// Values between 1 and 8.
-			float y = (int)(Random.value * (rows-2)+1);
+			// Values between 1 and the number of rows-1.
+			float x = (int)(Random.value * (rows-2)+1);
+			// Values between 1 and the number of columns-1.
+			float y = (int)(Random.value * (columns-2)+1);
 
 			// -1 for the z-axis allows the sprite to be displayed in front of the tile.
 			Vector3 location = new Vector3(x,y,-1);
 
+			// Checks if the random position hasn't been added already.
 			if(positions.Contains(location) == false){
+				// Add the position to the list.
 				positions.Add(location);
-				Instantiate (RandomItem(), location, Quaternion.identity);
+				// Instantiate the new item GameObject.
+				GameObject newItem = Instantiate (RandomItem(), location, Quaternion.identity) as GameObject;
+				// Add the item to a parent GameObject to reduce cluster in the hierarchy.
+				newItem.transform.SetParent(boardItems);
 			}
 		}
 	}
