267bd3f073d2c59b5e14f29ff8441d1a1bce244f
olearytr <oleary54321@gmail.com>
2015-10-07 19:32:11 -0400

    Added 10 unique maps, level selector function, and a pit tile
    +Added Pit Tile
    +Added 10 Unique maps (BoardManager.cs)
    +Added Level Selector in BoardManager.cs
    +Added variable 'level' in GameManager.cs
    (Note: this will be used for telling the board what level to load)
    
    As of right now,  there are 10 levels each with a ladder to proceed to
    the next level. At the moment, the ladder will not transition the player
    to the next level (I plan on putting that in Thursday or Friday). So if
    you want to check each level design, you have to manually change the
    'level' in GameManager.cs

--- Assets/C#Scripts/GameManager.cs
+++ Assets/C#Scripts/GameManager.cs
@@ -4,11 +4,28 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 
 	public GameObject player;
+	public GameObject cynthia;
+    public BoardManager boardScript;
+    private int level = 10;
+
+    //public static GameManager instance = null;
 
 	// Use this for initialization
 	void Start () {
+
+    //    if (instance == null)
+    //        instance = this;
+    //    else if (instance != this)
+    //        Destroy(gameObject);
+
+    //    DontDestroyOnLoad(gameObject);
+
 		Instantiate (player);
-	}
+		Instantiate (cynthia);
+
+        boardScript = GetComponent<BoardManager>();
+        boardScript.LevelSelector(level);
+	} 
 	
 	// Update is called once per frame
 	void Update () {
