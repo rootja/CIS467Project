592984bacd2d16cd67fe661dd36beaac4b2d9540
olearytr <oleary54321@gmail.com>
2015-10-08 22:47:20 -0400

    Attempted Level Transitions, Had issues with Player not moving
    +Added new functions to GameManager.cs
    OnLevelWasLoaded should be called each time the level loads and it
    increases the level counter to move the board to the next map then moves
    on to Init. This one just tells the board to load the map according to
    the level.
    
    +Added 2 functions in GridAura.cs (I think this is where I should put
    them, I could be wrong)  I put in OnTriggerEnter2D which checks if the
    tile it collided with has the Exit tag and if it does it calls Restart.
    Restart reloads the scene and it should then increment the level using
    the new functions above.
    
    At the moment, I could not test if these work due to an issue with the
    player being unable to move

--- Assets/C#Scripts/GameManager.cs
+++ Assets/C#Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour {
 	public GameObject player;
 	public GameObject cynthia;
     public BoardManager boardScript;
-    private int level = 10;
+    private int level = 1;
 
     //public static GameManager instance = null;
 
@@ -24,8 +24,22 @@ public class GameManager : MonoBehaviour {
 		Instantiate (cynthia);
 
         boardScript = GetComponent<BoardManager>();
+        Init();
+	}
+
+    //Called each time a level was loaded
+    void OnLevelWasLoaded(int index)
+    {
+        //Add one to our level number.
+        level++;
+        //Call Init to initialize the level.
+        Init();
+    }
+
+    void Init()
+    {
         boardScript.LevelSelector(level);
-	} 
+    }
 	
 	// Update is called once per frame
 	void Update () {
