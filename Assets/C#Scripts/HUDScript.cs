02145545e4ebbcee85391e6d9331841eda65f4de
mieschst <mieschst@mail.gvsu.edu>
2015-11-01 15:18:23 -0500

    Player Health Bar Added
    The HUD now displays a health bar for the player.

--- Assets/C#Scripts/HUDScript.cs
+++ Assets/C#Scripts/HUDScript.cs
@@ -9,6 +9,11 @@ public class HUDScript : MonoBehaviour {
 	public Text SkillText;
 	public Text RupeeText;
 
+	//This slider manages the player's health bar
+	public Slider HealthSlider;
+	public Image SliderBackground;
+	public Image SliderForeground;
+
 	// Use this for initialization
 	void Start () {
 		//The following block sets up the HUD
@@ -18,24 +23,59 @@ public class HUDScript : MonoBehaviour {
 		SkillText.enabled = true;
 		RupeeText = GameObject.Find ("RupeeText").GetComponent<Text> ();
 		RupeeText.enabled = true;
+		//This part sets up the player's health bar
+		HealthSlider.enabled = true;
+		SliderBackground.enabled = true;
+		SliderForeground.enabled = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		CheckIfPaused ();
+		AdjustPlayerHealthBar ();
+	}
+
+	void CheckIfPaused(){
+
 		if (PauseScript.isKeysEnabled) {
+			//Enables the HUD's labels
 			HealthText.enabled = true;
-			HealthText.text = "HEALTH: " + Player.health;
+			HealthText.text = "HEALTH: ";
 			SkillText.enabled = true;
 			SkillText.text = "PLAYER LEVEL: " + Player.playerLevel;
 			RupeeText.enabled = true;
 			RupeeText.text = "RUPEES: " + Player.currency;
+
+			//Enables the HUD's player health bar
+			HealthSlider.enabled = true;
+			SliderBackground.enabled = true;
+			SliderForeground.enabled = true;
+
 		} else {
+			//Disables the HUD's labels
 			HealthText.enabled = false;
 			SkillText.enabled = false;
 			RupeeText.enabled = false;
+			//Disables the HUD's player health bar
+			HealthSlider.enabled = false;
+			SliderBackground.enabled = false;
+			SliderForeground.enabled = false;
 		}
-
-
 	}
+
+	void AdjustPlayerHealthBar(){
+
+		HealthSlider.maxValue = Player.maxhealth;
+		
+		if (Player.health <= 0) {
+			HealthSlider.value = 0;
+			SliderForeground.enabled = false;
+		} else if (Player.health >= Player.maxhealth) {
+			HealthSlider.value = HealthSlider.maxValue;
+		} else {
+			HealthSlider.value = Player.health;
+		}
+	}
+
 }
